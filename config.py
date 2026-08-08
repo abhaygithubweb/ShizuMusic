e4fbc10cca248e7e57cad5dbb5470505cb60e190
abhaygithubweb <164343100+abhaygithubweb@users.noreply.github.com>
2026-08-08 17:16:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ NSFW_THRESHOLDS = {
 
 #── Start ───────────────────────────────────────────────────────────────────────
 START_ANIMATIONS = [
-   https://gxtusqitetsemwjdtvvq.supabase.co/storage/v1/object/public/photos/1786186762078-0ie20e.jpg,
+   "https://gxtusqitetsemwjdtvvq.supabase.co/storage/v1/object/public/photos/1786186762078-0ie20e.jpg",
 ]
 
 # ── Limits ────────────────────────────────────────────────────────────────────
